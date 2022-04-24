422ea870bde0cbeb1ee75f6c3f25a2dab85c5025
dina <dina_dal@localhost>
2022-04-25 02:06:10 +0300

    Login and reads from file

---
+++ ATM/src/Login.java
@@ -0,0 +1,93 @@
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.lang.model.element.NestingKind;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Login implements ActionListener{
+
+	
+	private static JLabel userLabel;
+	private static JTextField userText;
+	private static JLabel passwordLabel;
+	private static JPasswordField passwordText;
+	private static JButton button;
+	private static JLabel success;
+	
+	public static void main(String[] args) {
+		JPanel panel = new JPanel();
+		
+		JFrame frame = new JFrame();
+		frame.setSize(350, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(panel);
+		
+		panel.setLayout(null);
+		
+		userLabel = new JLabel("User");
+		userLabel.setBounds(10, 20, 80, 25);
+		panel.add(userLabel);
+		
+		userText = new JTextField();
+		userText.setBounds(100, 20, 165, 25);
+		panel.add(userText);
+		
+		passwordLabel = new JLabel("Password");
+		passwordLabel.setBounds(10, 50, 80, 25);
+		panel.add(passwordLabel);
+		
+		passwordText = new JPasswordField();
+		passwordText.setBounds(100, 50, 165, 25);
+		panel.add(passwordText);
+		
+		button = new JButton("Login");
+		button.setBounds(10,80,80,25);
+		button.addActionListener(new Login());
+		panel.add(button);
+		
+		success = new JLabel("");
+		success.setBounds(10, 110, 300, 25);
+		panel.add(success);
+		success.setText(null);
+		
+		
+		frame.setVisible(true);
+
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String user = userText.getText();
+		String password = passwordText.getText();
+       
+
+        try {
+			Scanner in = new Scanner(new File("./src/RounterInfo.txt"));
+			while(in.hasNextLine()){
+				 //if there is a line, print it out
+				 String line = in.nextLine();
+				 System.out.println(line);
+			 }
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        
+		if(user.equals("Dina") && password.equals("123")) {
+			success.setText("login Successful");
+			
+		}
+	}
+}
