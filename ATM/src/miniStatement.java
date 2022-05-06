95874c2d8e3562868e93c89938c280f35f9cfaf3
shayma <shaymaadabbagh@gmail.com>
2022-05-06 11:43:12 +0300

    update

--- ATM/src/miniStatement.java
+++ ATM/src/miniStatement.java
@@ -9,12 +9,11 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
 
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+
 import javax.swing.JPanel;
 
 import javax.swing.JTextField;
@@ -23,25 +22,26 @@ import javax.swing.border.EmptyBorder;
 public class miniStatement  extends JFrame implements ActionListener{
 	private JPanel contentPane;
 
-	public miniStatement() {
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//setBounds(200, 200, 800, 300);
-		contentPane = new JPanel();
-		contentPane.setToolTipText("");
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		JFrame frame = new JFrame ("Ministatement");
-		String [] [] data= {
-				{"001","Shaymaa", "200", "1000"}
-		};
-		
-		String[] columnNames = {"Account Number", "Name" , "Amount", "Balance"};
-		JTable table = new JTable(data, columnNames);
-		frame.add(new JScrollPane(table));
-		frame.setVisible(true);
-		frame.setSize(500,500);
+		public miniStatement() {
+			
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			//setBounds(200, 200, 800, 300);
+			contentPane = new JPanel();
+			contentPane.setToolTipText("");
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			setContentPane(contentPane);
+			contentPane.setLayout(null);
+			JFrame frame = new JFrame ("Ministatement");
+			String [] [] data= {
+					{"001","Shaymaa", "200", "1000"}
+			};
+			
+			String[] columnNames = {"Account Number", "Name" , "Amount", "Balance"};
+			JTable table = new JTable(data, columnNames);
+			frame.add(new JScrollPane(table));
+			frame.setVisible(true);
+			frame.setSize(500,500);
 		
 	}
 	
@@ -50,8 +50,19 @@ public class miniStatement  extends JFrame implements ActionListener{
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					miniStatement frame = new miniStatement();
-					//frame.setVisible(false);
+					Scanner sc = new Scanner(new BufferedReader(new FileReader("/src/RounterInfo.txt")));
+				      int rows = 4;
+				      int columns = 4;
+				      int [][] myArray = new int[rows][columns];
+				      while(sc.hasNextLine()) {
+				         for (int i=0; i<myArray.length; i++) {
+				            String[] line = sc.nextLine().trim().split(" ");
+				            for (int j=0; j<line.length; j++) {
+				               myArray[i][j] = Integer.parseInt(line[j]);
+				            }
+				         }
+				      }
+				      System.out.println(Arrays.deepToString(myArray));
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
