7518f4ba03deb246a1d39c0702d3d1156c52e558
dinadal <75212333+dinadal@users.noreply.github.com>
2022-05-07 08:19:27 +0300

    Update MainMenu.java
    Organized the code,fixed the cancel,enter and clear button, added comments

--- ATM/src/MainMenu.java
+++ ATM/src/MainMenu.java
@@ -15,11 +15,9 @@ import javax.swing.ImageIcon;
 public class MainMenu {
 
 	public JFrame frame;
-	private JTextField JLabDisplay;
+	private JTextField jLabDisplay;
 
-	/**
-	 * Launch the application.
-	 */
+	//Launch the application
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -33,40 +31,58 @@ public class MainMenu {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
+	//Create the application
 	public MainMenu() {
-		
+		//Setting the size of the frame
 		frame = new JFrame();
 		frame.setBounds(100, 100, 600, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		//Setting the size of the panel
 		JPanel panel = new JPanel();
 		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		panel.setBounds(22, 6, 172, 188);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		JButton btnBalance = new JButton("create credit card");
-		btnBalance.addActionListener(new ActionListener() {
-			
-			@Override  //Takes the user to the withdraw frame
-			public void actionPerformed(ActionEvent e) {
-				frame.dispose();
-				creditCard panel = new creditCard();
-				panel.setVisible(true);
-				panel.setLocation(400,250);
-			}
-			
-		});
+		//Properties of panel_1
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.LIGHT_GRAY);
+		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_1.setBounds(206, 6, 200, 188);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+		
+		//Properties of jLabDisplay
+		jLabDisplay = new JTextField();
+		jLabDisplay.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		jLabDisplay.setBounds(6, 22, 188, 133);
+		panel_1.add(jLabDisplay);
+		jLabDisplay.setColumns(10);
+		
+		//Properties of panel_2
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_2.setBounds(414, 6, 180, 188);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+		
+		//Properties of panel_3
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_3.setBounds(16, 206, 578, 356);
+		frame.getContentPane().add(panel_3);
+		panel_3.setLayout(null);
+		
+		//Balance button
+		JButton btnBalance = new JButton("Balance");
 		btnBalance.setBounds(25, 29, 117, 52);
 		panel.add(btnBalance);
 		
+		//Withdraw button
 		JButton btnWithdraw = new JButton("Withdraw");
 		btnWithdraw.addActionListener(new ActionListener() {
-					
 					@Override  //Takes the user to the withdraw frame
 					public void actionPerformed(ActionEvent e) {
 						frame.dispose();
@@ -79,25 +95,7 @@ public class MainMenu {
 		btnWithdraw.setBounds(25, 105, 117, 52);
 		panel.add(btnWithdraw);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.LIGHT_GRAY);
-		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_1.setBounds(206, 6, 200, 188);
-		frame.getContentPane().add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabDisplay = new JTextField();
-		JLabDisplay.setFont(new Font("Lucida Grande", Font.BOLD, 14));
-		JLabDisplay.setBounds(6, 22, 188, 133);
-		panel_1.add(JLabDisplay);
-		JLabDisplay.setColumns(10);
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_2.setBounds(414, 6, 180, 188);
-		frame.getContentPane().add(panel_2);
-		panel_2.setLayout(null);
-		
+		//Deposite button
 		JButton btnDeposit = new JButton("Deposit");
 		btnDeposit.setBounds(24, 25, 120, 52);
 		btnDeposit.addActionListener(new ActionListener() {
@@ -111,150 +109,150 @@ public class MainMenu {
 	});
 		panel_2.add(btnDeposit);
 		
+		//Mini statement button
 		JButton btnMiniState = new JButton("Mini Statement");
 		btnMiniState.setBounds(24, 101, 120, 52);
 		panel_2.add(btnMiniState);
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_3.setBounds(16, 206, 578, 356);
-		frame.getContentPane().add(panel_3);
-		panel_3.setLayout(null);
+		//Adding the number buttons 0-9
 		
+		//Button for number 0
+		JButton jBtn0 = new JButton("");
+		jBtn0.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/0.jpeg")));
+		jBtn0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = jLabDisplay.getText() + "0";
+				jLabDisplay.setText(enterNumber);
+			}
+		});
+		jBtn0.setBounds(184, 251, 101, 64);
+		panel_3.add(jBtn0);
+		
+		//Button for number 1
+		JButton jBtn1 = new JButton("");
+		jBtn1.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/1.jpeg")));
+		jBtn1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = jLabDisplay.getText() + "1";
+				jLabDisplay.setText(enterNumber);
+			}
+		});
+		jBtn1.setBounds(71, 25, 101, 64);
+		panel_3.add(jBtn1);
+		
+		//Button for number 2
 		JButton jBtn2 = new JButton("");
 		jBtn2.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/2.jpeg")));
 		jBtn2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "2";
-				JLabDisplay.setText(enterNumber);
+				String enterNumber = jLabDisplay.getText() + "2";
+				jLabDisplay.setText(enterNumber);
 			}
 		});
 		jBtn2.setBounds(184, 25, 101, 64);
 		panel_3.add(jBtn2);
 		
+		//Button for number 3
 		JButton jBtn3 = new JButton("");
 		jBtn3.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/3.jpeg")));
 		jBtn3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "3";
-				JLabDisplay.setText(enterNumber);
+				String enterNumber = jLabDisplay.getText() + "3";
+				jLabDisplay.setText(enterNumber);
 			}
 		});
 		jBtn3.setBounds(297, 25, 101, 64);
 		panel_3.add(jBtn3);
 		
+		//Button for number 4
+		JButton jBtn4 = new JButton("");
+		jBtn4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = jLabDisplay.getText() + "4";
+				jLabDisplay.setText(enterNumber);
+			}
+		});
+		jBtn4.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/4.jpeg")));
+		jBtn4.setBounds(71, 101, 101, 64);
+		panel_3.add(jBtn4);
+		
+		//Button for number 5
 		JButton jBtn5 = new JButton("");
 		jBtn5.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/5.jpeg")));
 		jBtn5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "5";
-				JLabDisplay.setText(enterNumber);
+				String enterNumber = jLabDisplay.getText() + "5";
+				jLabDisplay.setText(enterNumber);
 			}
 		});
 		jBtn5.setBounds(184, 101, 101, 64);
 		panel_3.add(jBtn5);
 		
-		JButton jBtn4 = new JButton("");
-		jBtn4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "4";
-				JLabDisplay.setText(enterNumber);
-			}
-		});
-			
-		jBtn4.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/4.jpeg")));
-		jBtn4.setBounds(71, 101, 101, 64);
-		panel_3.add(jBtn4);
-		
+		//Button for number 6
 		JButton jBtn6 = new JButton("");
 		jBtn6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "6";
-				JLabDisplay.setText(enterNumber);
+				String enterNumber = jLabDisplay.getText() + "6";
+				jLabDisplay.setText(enterNumber);
 			}
 		});
 		jBtn6.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/6.jpeg")));
 		jBtn6.setBounds(297, 101, 101, 64);
 		panel_3.add(jBtn6);
 		
-		JButton jBtnClear = new JButton("");
-		jBtnClear.setIcon(new ImageIcon("/Users/dina_dal/Desktop/Icons/clear.jpeg"));
-		jBtnClear.setBounds(410, 140, 101, 64);
-		panel_3.add(jBtnClear);
-		
-		JButton jBtn8 = new JButton("");
-		jBtn8.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/8.jpeg")));
-		jBtn8.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "8";
-				JLabDisplay.setText(enterNumber);
-			}
-		});
-		jBtn8.setBounds(184, 176, 101, 64);
-		panel_3.add(jBtn8);
-		
+		//Button for number 7
 		JButton jBtn7 = new JButton("");
 		jBtn7.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/7.jpeg")));
 		jBtn7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "7";
-				JLabDisplay.setText(enterNumber);
+				String enterNumber = jLabDisplay.getText() + "7";
+				jLabDisplay.setText(enterNumber);
 			}
 		});
 		jBtn7.setBounds(71, 176, 101, 64);
 		panel_3.add(jBtn7);
 		
+		//Button for number 8
+		JButton jBtn8 = new JButton("");
+		jBtn8.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/8.jpeg")));
+		jBtn8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String enterNumber = jLabDisplay.getText() + "8";
+				jLabDisplay.setText(enterNumber);
+			}
+		});
+		jBtn8.setBounds(184, 176, 101, 64);
+		panel_3.add(jBtn8);
+		
+		//Button for number 9
 		JButton jBtn9 = new JButton("");
 		jBtn9.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/9.jpeg")));
 		jBtn9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "9";
-				JLabDisplay.setText(enterNumber);
+				String enterNumber = jLabDisplay.getText() + "9";
+				jLabDisplay.setText(enterNumber);
 			}
 		});
 		jBtn9.setBounds(297, 176, 101, 64);
 		panel_3.add(jBtn9);
 		
+		//Clear button
+		JButton jBtnClear = new JButton("");
+		jBtnClear.setIcon(new ImageIcon("/Users/dina_dal/Desktop/Icons/clear.jpeg"));
+		jBtnClear.setBounds(410, 140, 101, 64);
+		panel_3.add(jBtnClear);
+		
+		//Enter button
 		JButton jBtnEnter = new JButton("");
 		jBtnEnter.setIcon(new ImageIcon("/Users/dina_dal/Desktop/Icons/enter.jpeg"));
 		jBtnEnter.setBounds(410, 230, 101, 64);
 		panel_3.add(jBtnEnter);
 		
-		JButton jBtn0 = new JButton("");
-		jBtn0.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/0.jpeg")));
-		jBtn0.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "0";
-				JLabDisplay.setText(enterNumber);
-			}
-		});
-		jBtn0.setBounds(184, 251, 101, 64);
-		panel_3.add(jBtn0);
-		
-		JButton btnNewButton_1_3 = new JButton("");
-		btnNewButton_1_3.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/empty.jpeg")));
-		btnNewButton_1_3.setBounds(71, 251, 101, 64);
-		panel_3.add(btnNewButton_1_3);
-		
-		JButton jBtn1 = new JButton("");
-		jBtn1.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/1.jpeg")));
-		jBtn1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String enterNumber = JLabDisplay.getText() + "1";
-				JLabDisplay.setText(enterNumber);
-			}
-		});
-		jBtn1.setBounds(71, 25, 101, 64);
-		panel_3.add(jBtn1);
-		
+		//Cancel button
 		JButton jBtnCancel = new JButton("");
 		jBtnCancel.setIcon(new ImageIcon("/Users/dina_dal/Desktop/Icons/cancel.jpeg"));
 		jBtnCancel.setBounds(410, 45, 101, 64);
 		panel_3.add(jBtnCancel);
-		
-		JButton btnNewButton_1_3_1 = new JButton("");
-		btnNewButton_1_3_1.setIcon(new ImageIcon(MainMenu.class.getResource("/Icons/empty.jpeg")));
-		btnNewButton_1_3_1.setBounds(297, 252, 101, 64);
-		panel_3.add(btnNewButton_1_3_1);
 	}
+		
 }
